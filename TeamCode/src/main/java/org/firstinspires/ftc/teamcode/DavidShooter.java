602cefbfe53ef580c2619fb9185a5e26ce1114ed
jamespec <38446091+jamespec@users.noreply.github.com>
2020-11-16 22:24:53 -0500

    Setup works for onBotJava, see gradle tasks in build.gradle. These include: push, pull to send and get OpModes to OnBotJava I disabled lint, we should probably figure out why it is returning an error on build.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DavidShooter.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp
+public class DavidShooter extends LinearOpMode {
+    
+    DcMotorEx shooter;
+    Servo front, back;
+    double encoder, rpm;
+
+    // todo: write your code here
+    @Override
+    public void runOpMode(){
+        shooter = hardwareMap.get( DcMotorEx.class, "shooter" );
+        shooter.setZeroPowerBehavior( DcMotorEx.ZeroPowerBehavior.FLOAT );
+        shooter.setDirection( DcMotorEx.Direction.REVERSE );
+
+        
+        front = hardwareMap.get( Servo.class, "front" );
+        back = hardwareMap.get( Servo.class, "back" );
+         front.setPosition(.5);
+         back.setPosition(.5);
+        waitForStart();
+        
+        while(opModeIsActive()){
+            shooter.setPower( gamepad1.left_stick_y * 2.0 / 3.0);
+        
+            telemetry.addData("Test","Hello" );
+            encoder = shooter.getVelocity();
+            rpm = encoder*(60/28);
+            telemetry.addData("RPM",rpm);
+            
+            if(rpm >=5000){
+                front.setPosition(.2);
+                back.setPosition(.2);
+                
+            }else{
+                front.setPosition(.5);
+                back.setPosition(.5);
+            }
+
+            
+            telemetry.update();
+            
+        }
+    }
+}
